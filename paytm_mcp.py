fe95c968daeeab85a85c267164dafa50f05d6823
Shivam garg <shivam3.garg@paytm.com>
2025-06-19 19:10:18 +0530

    fix server issue

--- paytm_mcp.py
+++ paytm_mcp.py
@@ -311,8 +311,9 @@ def fetch_order_list(
         logger.error(f"Failed to fetch order list: {str(e)}")
         return str(e)
     
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    mcp.run(host="0.0.0.0", port=port,transport="sse")
+app = mcp.asgi_app()   
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8080))
+#    mcp.run(host="0.0.0.0", port=port,transport="sse")
 
 
